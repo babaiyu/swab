8386df50fcb406bc6f9a42cddfc980414b3a7f29
babaiyu <bayuputra9a@gmail.com>
2020-12-02 17:38:12 +0700

    added kotak-kotak background

--- src/functions/greetings.js
+++ src/functions/greetings.js
@@ -1,6 +1,18 @@
 let greeting = document.getElementById('mygreetings');
+
 function greetings() {
-  greeting.innerHTML = "Selamat hari Jum'at";
+  let day = new Date();
+  let weekday = new Array(7);
+  weekday[0] = 'Minggu';
+  weekday[1] = 'Senin';
+  weekday[2] = 'Selasa';
+  weekday[3] = 'Rabu';
+  weekday[4] = 'Kamis';
+  weekday[5] = "Jum'at";
+  weekday[6] = 'Sabtu';
+
+  let theDay = weekday[day.getDay()];
+  greeting.innerHTML = `${theDay}`;
 }
 
 export default greetings;
